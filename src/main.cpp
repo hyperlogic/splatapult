62edb21ecf2ea54a84874ad67139761baed0e33c
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2023-09-25 16:39:09 -0700

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@ static SDL_Window *window = NULL;
 static SDL_GLContext gl_context;
 static SDL_Renderer *renderer = NULL;
 
-void render() {
+void render()
+{
     SDL_GL_MakeCurrent(window, gl_context);
     glm::vec4 clearColor(0.0f, 0.4f, 0.0f, 1.0f);
     clearColor.r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
@@ -22,18 +23,20 @@ void render() {
 }
 
 
-int SDLCALL watch(void *userdata, SDL_Event* event) {
-
-    if (event->type == SDL_APP_WILLENTERBACKGROUND) {
+int SDLCALL watch(void *userdata, SDL_Event* event)
+{
+    if (event->type == SDL_APP_WILLENTERBACKGROUND)
+    {
         quitting = true;
     }
 
     return 1;
 }
 
-int main(int argc, char *argv[]) {
-
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0) {
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0)
+    {
         SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
         return 1;
     }
@@ -43,17 +46,21 @@ int main(int argc, char *argv[]) {
     gl_context = SDL_GL_CreateContext(window);
 
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (!renderer) {
+	if (!renderer)
+    {
         SDL_Log("Failed to SDL Renderer: %s", SDL_GetError());
 		return -1;
 	}
 
     SDL_AddEventWatch(watch, NULL);
 
-    while (!quitting) {
+    while (!quitting)
+    {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
                 quitting = true;
             }
         }
