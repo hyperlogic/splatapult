7e0ee046f0799435dd7c72df23162ba7777fa3d3
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2023-11-12 10:43:29 -0800

    added comments

--- shader/splat_geom.glsl
+++ shader/splat_geom.glsl
@@ -48,14 +48,22 @@ void main()
         return;
     }
 
+    // k is the desired value we want to bound.
+    // where k = dot(d, conv2Dinv * d);
+    // or k = x (a*x + c*y) + y (b*x + d*y)
+    // We solve for the x that will give us one y solution.
+    // and solve for the y that will give us one x solution.
+    // we use these x & y's to build an axis aligned bounding box
+    // that is guarrenteed to enclose k = 8.
     float k = 8.0f;
     float a = cov2Dinv4.x;
     float b = cov2Dinv4.z;
     float c = cov2Dinv4.y;
     float d = cov2Dinv4.w;
     float denom = sqrt(-(b * b) - 2 * b * c - (c * c) + 4 * a * d);
-    float xx = 2.0f * sqrt(d) * sqrt(k) / denom;
-    float yy = 2.0f * sqrt(a) * sqrt(k) / denom;
+    float sqrtk = sqrt(k);
+    float xx = 2.0f * sqrt(d) * sqrtk / denom;
+    float yy = 2.0f * sqrt(a) * sqrtk / denom;
     vec2 o[4];
     o[0] = vec2(xx, (-(b * xx) - (c * xx)) / (2.0f * d));
     o[1] = vec2(((-b * yy) - (c * yy)) / (2.0f * a), yy);
@@ -74,9 +82,8 @@ void main()
     float w = gl_in[0].gl_Position.w;
     for (int i = 0; i < 4; i++)
     {
+        // transform offset back into clip space, and apply it to gl_Position.
         offset = offsets[i];
-
-        // transform that offset back into clip space, and apply it to gl_Position.
         offset.x *= (2.0f / WIDTH) * w;
         offset.y *= (2.0f / HEIGHT) * w;
 
