471870f87752901e2dba393bde20ceeabfc21395
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-01-27 09:33:09 -0800

    Added invoke script
    To help make release zip files

---
+++ tasks.py
@@ -0,0 +1,30 @@
+from invoke import task
+import os
+import shutil
+
+
+RELEASE_NAME = "splatapult-0.1-x64"
+VCPKG_DIR = os.getenv("VCPKG_DIR")
+
+
+@task
+def build(c):
+    c.run("rm -rf build")
+    c.run("mkdir build")
+    with c.cd("build"):
+        c.run(
+            f'cmake -DSHIPPING=ON -DCMAKE_TOOLCHAIN_FILE="{VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake" ..'
+        )
+        c.run(f"cmake --build . --config Release")
+
+@task
+def archive(c):
+    shutil.make_archive(RELEASE_NAME, "zip", "build/Release")
+
+@task
+def deploy(c):
+    c.run(f"cp {RELEASE_NAME}.zip ../hyperlogic.github.io/files")
+    with c.cd("../hyperlogic.github.io"):
+        c.run(f"git add files/{RELEASE_NAME}.zip")
+        c.run(f'git commit -m "Automated deploy of {RELEASE_NAME}"')
+        c.run("git push")
